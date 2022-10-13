2ba952aae18a5c949683110987e77195741efe2f
Pavithran <pavithrank02@gmail.com>
2022-10-13 19:07:12 +0530

    handling counter value

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ const counterReducer = (state = {counter: 0}, action) => {
             counter: state.counter + 1,
             }
     }
-    if(action.type === 'deccrement') {
+    if(action.type === 'decrement') {
         return {
             counter: state.counter - 1,
             }
