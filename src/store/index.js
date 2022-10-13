1ab951b2fb188fc39bdf2cbed08ca23a90c50ddd
Pavithran <pavithrank02@gmail.com>
2022-10-13 21:07:01 +0530

    connecting to redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import {createStore } from 'redux';
 
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {counter: 0, showCounter: true}
 
-createSlice({
+const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
@@ -21,40 +21,42 @@ createSlice({
             state.showCounter = !state.showCounter;
         }
     }
+});
+const store = configureStore({
+    reducer: counterSlice.reducer
 })
-const counterReducer = (state = initialState, action) => {
-    if(action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-            }
-    }
-    if(action.type === 'increase') {
-        return {
-            counter: state.counter + action.value,
-            showCounter: state.showCounter
-            }
-    }
-    if(action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-            }
-    }
-
-    if(action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-
-    return state;
-
-}
-const store = createStore(counterReducer);
 
 export default store;
+// const counterReducer = (state = initialState, action) => {
+//     if(action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//             }
+//     }
+//     if(action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.value,
+//             showCounter: state.showCounter
+//             }
+//     }
+//     if(action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//             }
+//     }
+
+//     if(action.type === 'toggle') {
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         }
+//     }
+
+//     return state;
+
+// }
 // const counterSubscribe = () => {
 //     const latestState = store.getState();
 //     console.log(latestState)
